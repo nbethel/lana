4935203f8cbddc69fe29ab036e6154cb1bfe504c
nbethel <ipqb@iPQBs-MacBook-Pro.local>
2014-06-12 19:23:25 -0400

    Made tracking more dynamic/fixed bugs. Added animation for when sight is lost after chasing

--- Golem_May_30/Assets/Mode.cs
+++ Golem_May_30/Assets/Mode.cs
@@ -16,18 +16,18 @@ public class Mode : MonoBehaviour {
 	}
 
 	public void set0() {
-		Debug.Log ("Set0"); //For wandering
+		//Debug.Log ("Set0"); //For wandering
 		mode=0;
 		//AI.WorkingMemory.SetItem("mode", mode);
 	}
 	public void set1() {
-		Debug.Log ("Set1"); //For Tracking
+		//Debug.Log ("Set1"); //For Tracking
 		mode=1;
 		//AI.WorkingMemory.SetItem("mode", mode);
 
 	}
 	public void set2() {
-		Debug.Log ("Set2"); //For Chasing
+		//Debug.Log ("Set2"); //For Chasing
 		mode=2;
 		//AI.WorkingMemory.SetItem("mode", mode);
 	}
